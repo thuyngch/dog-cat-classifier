e2315d047c12042727e1f4b5b424f86caffe2f40
son <nguyenthengoc.1994@gmail.com>
2020-04-23 21:44:34 +0700

    update predict.py

--- predict.py
+++ predict.py
@@ -1,43 +1,47 @@
-import torch
+import torch, os
 from utils.helpers import *
 import warnings
 from PIL import Image
 from torchvision import transforms
-#from torchsummary import summary
+from torchsummary import summary
+from glob import glob
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+
+@torch.no_grad()
 def image_transform(imagepath):
-    test_transforms = transforms.Compose([transforms.Resize(255),
-                                      transforms.CenterCrop(224),
-                                      transforms.ToTensor(),
-                                      transforms.Normalize([0.485, 0.456, 0.406],
-                                                           [0.229, 0.224, 0.225])])
-    image = Image.open(imagepath)
-    imagetensor = test_transforms(image)
-    return imagetensor
+	test_transforms = transforms.Compose([
+		transforms.Resize(255),
+		transforms.CenterCrop(224),
+		transforms.ToTensor(),
+		transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+	image = Image.open(imagepath)
+	imagetensor = test_transforms(image)
+	return imagetensor
 
+@torch.no_grad()
+def predict(model, imagepath, device='cuda'):
+	image = image_transform(imagepath)
+	image1 = image[None,:,:,:].to(device)
+	ps = torch.exp(model(image1))
+	topconf, topclass = ps.topk(1, dim=1)
+	if topclass.item() == 1:
+		return {'class':'dog','confidence':str(topconf.item())}
+	else:
+		return {'class':'cat','confidence':str(topconf.item())}
 
-def predict(imagepath, verbose=False):
-    if not verbose:
-        warnings.filterwarnings('ignore')
-    model_path = './models/catvdog.pth'
-    try:
-        checks_if_model_is_loaded = type(model)
-    except:
-        model = load_model(model_path)
-    model.eval()
-    #summary(model, input_size=(3,244,244))
-    if verbose:
-        print("Model Loaded..")
-    image = image_transform(imagepath)
-    image1 = image[None,:,:,:]
-    ps=torch.exp(model(image1))
-    topconf, topclass = ps.topk(1, dim=1)
-    if topclass.item() == 1:
-        return {'class':'dog','confidence':str(topconf.item())}
-    else:
-        return {'class':'cat','confidence':str(topconf.item())}
+if __name__ == '__main__':
+	# Define
+	img_dir = "data"
+	model_path = 'models/catvdog.pth'
 
-#print(predict('data/dog1.jpeg'))
-#print(predict('data/cat1.jpeg'))
-#print(predict('data/dog2.jpeg'))
-#print(predict('data/cat2.jpeg'))
+	# Build model
+	model = load_model(model_path)
+	model.eval().cuda()
+	summary(model, input_size=(3,244,244))
+
+	# Infer images
+	image_files = sorted(glob(os.path.join(img_dir, "*.*")))
+	for image_file in image_files:
+		result = predict(model, image_file)
+		print("{}: {}".format(image_file, result))
